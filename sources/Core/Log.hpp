21cc328c663a8136db984cae17feec187728b30f
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2012-07-13 19:23:52 +0700

    Info record with 11 params.

--- sources/Core/Log.hpp
+++ sources/Core/Log.hpp
@@ -1280,10 +1280,40 @@ namespace Log {
 				const Param7 &param7,
 				const Param8 &param8,
 				const Param9 &param9,
-				const Param9 &param10)
+				const Param10 &param10)
 			throw() {
 		Detail::AppendRecord(
 			str, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10);
+	}
+
+	template<
+		typename Param1,
+		typename Param2,
+		typename Param3,
+		typename Param4,
+		typename Param5,
+		typename Param6,
+		typename Param7,
+		typename Param8,
+		typename Param9,
+		typename Param10,
+		typename Param11>
+	inline void Info(
+				const char *str,
+				const Param1 &param1,
+				const Param2 &param2,
+				const Param3 &param3,
+				const Param4 &param4,
+				const Param5 &param5,
+				const Param6 &param6,
+				const Param7 &param7,
+				const Param8 &param8,
+				const Param9 &param9,
+				const Param9 &param10,
+				const Param9 &param11)
+			throw() {
+		Detail::AppendRecord(
+			str, param1, param2, param3, param4, param5, param6, param7, param8, param9, param10, param11);
 	}
 
 }
