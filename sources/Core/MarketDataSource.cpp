f0c93f318c0a87d7e36ba11e6ee04653d06c0aaf
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2012-07-15 13:35:56 +0700

    MarketDataSource interface.

---
+++ sources/Core/MarketDataSource.cpp
@@ -0,0 +1,34 @@
+/**************************************************************************
+ *   Created: 2012/07/15 13:22:47
+ *    Author: Eugene V. Palchukovsky
+ *    E-mail: eugene@palchukovsky.com
+ * -------------------------------------------------------------------
+ *   Project: Trading Robot
+ **************************************************************************/
+
+#include "Prec.hpp"
+#include "MarketDataSource.hpp"
+
+//////////////////////////////////////////////////////////////////////////
+
+MarketDataSource::Error::Error(const char *what) throw()
+		: Exception(what) {
+	//...//
+}
+
+MarketDataSource::ConnectError::ConnectError()
+		: Error("Failed to connect to Market Data Source") {
+	//...//
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+MarketDataSource::MarketDataSource() {
+	//...//
+}
+
+MarketDataSource::~MarketDataSource() {
+	//...//
+}
+
+//////////////////////////////////////////////////////////////////////////
--- sources/Core/MarketDataSource.hpp
+++ sources/Core/MarketDataSource.hpp
@@ -1,43 +1,52 @@
 /**************************************************************************
- *   Created: 2012/6/6/ 20:00
+ *   Created: 2012/07/15 13:20:35
  *    Author: Eugene V. Palchukovsky
  *    E-mail: eugene@palchukovsky.com
  * -------------------------------------------------------------------
- *   Project: HighmanTradingRobot
+ *   Project: Trading Robot
  **************************************************************************/
 
 #pragma once
 
-#include "Core/MarketDataSource.hpp"
+class DynamicSecurity;
 
-class IqFeedClient : public MarketDataSource {
-
-public:
-
-	IqFeedClient();
-	virtual ~IqFeedClient();
+class MarketDataSource : private boost::noncopyable {
 
 public:
 
-	virtual void Connect();
+	class Error : public Exception {
+	public:
+		explicit Error(const char *what) throw();
+	};
+
+	class ConnectError : public Error {
+	public:
+		ConnectError() throw();
+	};
 
 public:
 
-	virtual void SubscribeToMarketData(boost::shared_ptr<DynamicSecurity>) const;
+	MarketDataSource();
+	virtual ~MarketDataSource();
+
+public:
+
+	virtual void Connect() = 0;
+
+public:
+
+	virtual void SubscribeToMarketData(boost::shared_ptr<DynamicSecurity>) const = 0;
 
 	virtual void RequestHistory(
 			boost::shared_ptr<DynamicSecurity>,
 			const boost::posix_time::ptime &fromTime)
-		const;
+		const
+		= 0;
 	virtual void RequestHistory(
 			boost::shared_ptr<DynamicSecurity>,
 			const boost::posix_time::ptime &fromTime,
 			const boost::posix_time::ptime &toTime)
-		const;
-
-private:
-
-	class Implementation;
-	Implementation *m_pimpl;
+		const
+		= 0;
 
 };
