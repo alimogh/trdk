b98491dc69b048349a45ff8e74bd882bb0b10464
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2012-08-31 02:27:29 +0700

    Removed space comments.

--- sources/Core/TradeSystem.hpp
+++ sources/Core/TradeSystem.hpp
@@ -24,46 +24,11 @@ namespace Trader {
 		typedef boost::int64_t OrderPrice;
 
 		enum OrderStatus {
-			/** Indicates that you have transmitted the order, but have not  yet
-			  * received confirmation that it has been accepted by the order
-			  * destination. NOTE: This order status is not sent by TWS and should
-			  * be explicitly set by the API developer when an order is submitted.
-			  * ORDER_STATUS_PENDIGN_SUBMIT
-			  */
-			/** Indicates that you have sent a request to cancel the order but have
-			  * not yet received cancel confirmation from the order destination. At
-			  * this point, your order is not confirmed canceled. You may still
-			  * receive an execution while your cancellation request is pending.
-			  * NOTE: This order status is not sent by TWS and should be explicitly
-			  * set by the API developer when an order is canceled.
-			  * ORDER_STATUS_PENDIGN_CANCEL
-			  */
-			/** Indicates that a simulated order type has been accepted by the IB
-			  * system and that this order has yet to be elected. The order is held
-			  * in the IB system until the election criteria are met. At that time
-			  * the order is transmitted to the order destination as specified.
-			  * ORDER_STATUS_PRE_SUBMITTED
-			  */
 			ORDER_STATUS_PENDIGN,
-			/** Indicates that your order has been accepted at the order destination
-			  * and is working.
-			  */
 			ORDER_STATUS_SUBMITTED,
-			/** Indicates that the balance of your order has been confirmed canceled
-			  * by the IB system. This could occur unexpectedly when IB or the
-			  * destination has rejected your order.
-			  */
 			ORDER_STATUS_CANCELLED,
-			/** Indicates that the order has been completely filled.
-			  */
 			ORDER_STATUS_FILLED,
-			/** Indicates that the order has been accepted by the system (simulated
-			  * orders) or an exchange (native orders) but that currently the order
-			  * is inactive due to system, exchange or other issues.
-			  */
 			ORDER_STATUS_INACTIVE,
-			/** Order placing error.
-			  */
 			ORDER_STATUS_ERROR
 		};
 
