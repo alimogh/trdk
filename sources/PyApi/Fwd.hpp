424691ba9e4f39932d03f172dac9975c25a29526
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2013-01-27 21:57:11 +0700

    Forgotten file for previous commit.

---
+++ sources/PyApi/Fwd.hpp
@@ -0,0 +1,25 @@
+/**************************************************************************
+ *   Created: 2013/01/27 20:38:47
+ *    Author: Eugene V. Palchukovsky
+ *    E-mail: eugene@palchukovsky.com
+ * -------------------------------------------------------------------
+ *   Project: Trading Robot Development Kit
+ *       URL: http://robotdk.com
+ **************************************************************************/
+
+#pragma once
+
+namespace Trader { namespace PyApi {
+
+	class Script;
+
+	class Strategy;
+	class StrategyExport;
+
+	class Service;
+	class ServiceExport;
+
+	template<typename Impl>
+	class SidePositionExport;
+
+} }
