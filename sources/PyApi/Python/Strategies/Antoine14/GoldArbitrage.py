bd8de28c0fa90147807c2247116765556f7d4a0f
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2013-05-12 21:49:31 +0700

    Strategy logic.

--- sources/PyApi/Python/Strategies/Antoine14/GoldArbitrage.py
+++ sources/PyApi/Python/Strategies/Antoine14/GoldArbitrage.py
@@ -1,47 +1,63 @@
 
+
 import trdk
 import time
 
+
 strategies = dict()
-lastIndex = -1
+
 
 class GoldArbitrage(trdk.Strategy):
 
     bars = None
-    lastSize = 0
+
 
     def __init__(self, param):
         super(self.__class__, self).__init__(param)
         strategies[self.security.symbol] = self
 
+
     def onServiceStart(self, service):
         self.bars = service
 
+
     def onServiceDataUpdate(self, service):
+        oppName = "GLD"
         if self.security.symbol == "GLD":
-            if strategies["DGL"].bars.size >= service.size:
-                self.checkEntry(service.size - 1)
-        elif self.security.symbol == "DGL":
-            if service.size < lastIndex:
-                return
-            gld = strategies["GLD"]
-            if gld.bars.size < service.size:
-                return
-            gld.checkEntry(service.size - 1)
-
-    def checkEntry(self, index):
-
-        if self.positions.count() > 0 or strategies["DGL"].positions.count() > 0:
+            oppName = "DGL"
+        oppService = strategies[oppName].bars
+        if oppService.size < 1:
             return
-            
-        gld = self.bars.getBarByIndex(index)
-        dgl = strategies["DGL"].bars.getBarByIndex(index)
+        lastBar = service.getBarByReversedIndex(0)
+        oppLastBar = oppService.getBarByReversedIndex(0)
+        # Trade restrictions:
+        # Obviously if DGL and/or GLD are not open for trading then do not trade.
+        # Possible that GLD opens before DGL in premarket trading
+        isOnline = oppLastBar.time == lastBar.time
+        self.log.debug(
+            "Online check: {0}({1}) == {2}({3}) = {4}"
+                .format(
+                    self.security.symbol,
+                    time.ctime(lastBar.time),
+                    oppName,
+                    time.ctime(oppLastBar.time),
+                    isOnline))
+        if isOnline == True:
+            if service.security.symbol == "GLD":
+                strategies["GLD"].checkEntry(lastBar, oppLastBar)
+            else:
+                strategies["GLD"].checkEntry(oppLastBar, lastBar)
 
-        self.lastSize = index
-            
+
+    def checkEntry(self, gld, dgl):
+
+        if self.positions.count() > 0:
+            self.log.debug("Has open positions...")
+            return
+
         ratioGe = 0
         if dgl.minBidPrice != 0:
-             ratioGe = float(gld.maxAskPrice) / float(dgl.minBidPrice)
+            ratioGe = float(gld.maxAskPrice) / float(dgl.minBidPrice)
 
         ratioLe = 0
         if dgl.maxAskPrice != 0:
@@ -59,8 +75,10 @@ class GoldArbitrage(trdk.Strategy):
             self.log.info('Opening positions by "Entry 1"...')
             gldPos = trdk.ShortPosition(self, 1, gld.maxAskPrice)
             dglPos = trdk.LongPosition(self, 1, dgl.minBidPrice)
-            gldPos.open(gldPos.openStartPrice)
-            dglPos.open(dglPos.openStartPrice)
+            gldPos.openAtMarketPrice()
+            dglPos.openAtMarketPrice()
+#            gldPos.open(gldPos.openStartPrice)
+#            dglPos.open(dglPos.openStartPrice)
         
         # If opening of 5 minute candlestick GLD(Bid)/DGL(Ask)<=2.842 .. 
         if ratioLe > 0 and ratioLe <= 2.842:
@@ -68,18 +86,33 @@ class GoldArbitrage(trdk.Strategy):
             self.log.info('Opening positions by "Entry 2"...')
             gldPos = trdk.LongPosition(self, 1, gld.minBidPrice)
             dglPos = trdk.ShortPosition(self, 1, dgl.maxAskPrice)
-            gldPos.open(gldPos.openStartPrice)
-            dglPos.open(dglPos.openStartPrice)
+            gldPos.openAtMarketPrice()
+            dglPos.openAtMarketPrice()
+#            gldPos.open(gldPos.openStartPrice)
+#            dglPos.open(dglPos.openStartPrice)
 
-    def tryToClosePosition(self, position):
+
+    def onPositionUpdate(self, position):
         if position.isOpened:
             # Profit Targets:
             # Sell positions @ GLD/DGL ratio of 2.847
-            closePrice = position.openStartPrice * 2.847
+            closePrice = int(position.openStartPrice * 2.847)
             self.log.debug(
-                "Closing {0} with {1}"
+                "Closing {0} with {1}."
                     .format(
                         self.security.symbol,
                         self.security.descalePrice(closePrice)))
             position.close(closePrice)
-        pass
+        elif position.isClosed:
+            # Risk Management:
+            # If one order gets filled without the other, sell
+            # immediately at market order.
+            self.log.debug(
+                "Closing all at market price."
+                    .format(
+                        self.security.symbol,
+                        self.security.descalePrice(closePrice)))
+            map(
+                lambda position: position.cancelAtMarketPrice(),
+                self.positions)
+
