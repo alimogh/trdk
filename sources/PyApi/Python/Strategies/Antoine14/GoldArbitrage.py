5002a50f90a01a095faaa592b471144cad46d5ad
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2013-09-03 22:14:21 +0700

    Fixed position close.

--- sources/PyApi/Python/Strategies/Antoine14/GoldArbitrage.py
+++ sources/PyApi/Python/Strategies/Antoine14/GoldArbitrage.py
@@ -1,5 +1,3 @@
-
-
 import trdk
 import time
 
@@ -43,6 +41,8 @@ class GoldArbitrage(trdk.Strategy):
     gldBars = None
     dglBars = None
 
+    ###########################################################################
+
     def onServiceStart(self, service):
         security = service.securities.find('GLD')
         if security is not None:
@@ -57,11 +57,35 @@ class GoldArbitrage(trdk.Strategy):
         assert False
 
     def onServiceDataUpdate(self, service):
-        if self.positions.count() > 0:
-            return
         if self._checkOnline() is False:
             return
-        self._checkEntry()
+        if self.positions.count() == 0:
+            self._checkEntry()
+        else:
+            map(
+                lambda position: self._checkExit(position),
+                self.positions)
+
+    def onPositionUpdate(self, position):
+        if position.isCompleted is False or position.isCanceled is True:
+            return
+        self.log.info('{0} closed first.'.format(position.security.symbol))
+        # Risk Management:
+        # If one order gets filled without the other, sell
+        # immediately at market order.
+        map(
+            lambda position: position.cancelAtMarketPrice(),
+            self.positions)
+
+    ###########################################################################
+
+    def findGld(self):
+        return self.findSecurity('GLD')
+
+    def findDgl(self):
+        return self.findSecurity('DGL')
+
+    ###########################################################################
 
     def _checkOnline(self):
         if self.gldBars.size < 1 or self.dglBars.size < 1:
@@ -72,20 +96,21 @@ class GoldArbitrage(trdk.Strategy):
         # Obviously if DGL and/or GLD are not open for trading then do not
         # trade. Possible that GLD opens before DGL in premarket trading
         isOnline = gld.time == dgl.time
-        self.log.debug(
-            'Online check: GLD({0}) == DGL({1}) = {2}'
-            .format(
-                time.ctime(gld.time),
-                time.ctime(dgl.time),
-                isOnline))
+        if self.positions.count() == 0:
+            self.log.debug(
+                'Online check: GLD({0}) == DGL({1}) = {2}'
+                .format(
+                    time.ctime(gld.time),
+                    time.ctime(dgl.time),
+                    isOnline))
         return isOnline
 
     def _checkEntry(self):
 
-        gld = self.findSecurity('GLD')
+        gld = self.findGld()
         gldBar = self.gldBars.getBarByReversedIndex(0)
 
-        dgl = self.findSecurity('DGL')
+        dgl = self.findDgl()
         dglBar = self.dglBars.getBarByReversedIndex(0)
 
         shortGldLongDlgRatio = 0
@@ -105,16 +130,18 @@ class GoldArbitrage(trdk.Strategy):
             longGldShortDgl = longGldShortDglRatio < (ratio * (0.999 / 100))
 
         self.log.debug(
-            "Entry 1: GLD(Ask {0}) / DGL(Bid {1}) = {2} - {3};"
-            " Entry 2: GLD(Bid {4}) / DGL(Ask {5}) = {6} - {7};"
+            "Entry 1: GLD(Ask {0}) / DGL(Bid {1}) = {2} == {3} - {4};"
+            " Entry 2: GLD(Bid {5}) / DGL(Ask {6}) = {7} == {8} - {9};"
             .format(
                 gld.descalePrice(gldBar.maxAskPrice),
                 dgl.descalePrice(dglBar.minBidPrice),
                 shortGldLongDlgRatio,
+                ratio,
                 shortGldLongDlg,
                 gld.descalePrice(gldBar.minBidPrice),
                 dgl.descalePrice(dglBar.maxAskPrice),
                 longGldShortDglRatio,
+                ratio,
                 longGldShortDgl))
 
         def calcQty(security, price):
@@ -176,37 +203,39 @@ class GoldArbitrage(trdk.Strategy):
             gldPos.open(gldPos.openStartPrice, orderDisplaySize)
             dglPos.open(dglPos.openStartPrice, orderDisplaySize)
 
-    def onPositionUpdate(self, position):
+    def _checkExit(self, position):
 
-        if position.isCompleted:
-            if position.isCanceled is not True:
-                self.log.info(
-                    '{0} closed first.'.format(position.security.symbol))
-            # Risk Management:
-            # If one order gets filled without the other, sell
-            # immediately at market order.
-            map(
-                lambda position: position.cancelAtMarketPrice(),
-                self.positions)
+        if position.isOpened is False or position.hasActiveCloseOrders is True:
+            return
 
-        elif position.isOpened and position.hasActiveCloseOrders is False:
+        if position.entryType == 'shortGldLongDlg':
+            # Exit trade when GLD (bid)/DGL (ask) = ratio
+            gldPrice = self.gldBars.getBarByReversedIndex(0).minBidPrice
+            dglPrice = self.dglBars.getBarByReversedIndex(0).maxAskPrice
+            message = 'Exit 1: GLD(Bid {0}) / DGL(Ask {1})'
+            message += ' = {2} == {3} - {4};'
+        else:
+            # Exit trade when GLD (ask)/DGL (bid) = ratio
+            assert position.entryType == 'longGldShortDgl'
+            gldPrice = self.gldBars.getBarByReversedIndex(0).maxAskPrice
+            dglPrice = self.dglBars.getBarByReversedIndex(0).minBidPrice
+            message = 'Exit 2: GLD(Ask {0}) / DGL(Bid {1})'
+            message += ' = {2} == {3} - {4};'
 
-            if position.entryType == 'shortGldLongDlg':
-                # Exit trade when GLD (bid)/DGL (ask) = ratio
-                gldPrice = self.gldBars.getBarByReversedIndex(0).minBidPrice
-                dglPrice = self.dglBars.getBarByReversedIndex(0).maxAskPrice
-                message = 'Exit 1: GLD(Bid {0}) / DGL(Ask {1}) = {2} - {3};"'
-            else:
-                # Exit trade when GLD (ask)/DGL (bid) = ratio
-                assert position.entryType == 'longGldShortDgl'
-                gldPrice = self.gldBars.getBarByReversedIndex(0).maxAskPrice
-                dglPrice = self.dglBars.getBarByReversedIndex(0).minBidPrice
-                message = 'Exit 2: GLD(Ask {0}) / DGL(Bid {1}) = {2} - {3};"'
+        currentRatio = float(gldPrice) / float(dglPrice)
+        currentRatio = round(currentRatio, 2)
+        isExitTime = currentRatio == ratio
 
-            isExitTime = gldPrice / dglPrice == ratio
-            self.log.info(
-                message.format(gldPrice, dglPrice, ratio, isExitTime))
-            if isExitTime is not True:
-                return
+        self.log.info(
+            message.format(
+                self.findGld().descalePrice(gldPrice),
+                self.findDgl().descalePrice(dglPrice),
+                currentRatio,
+                ratio,
+                isExitTime))
+        if isExitTime is not True:
+            return
 
-            position.closeAtMarketPrice(orderDisplaySize)
+        position.closeAtMarketPrice(orderDisplaySize)
+
+    ###########################################################################
