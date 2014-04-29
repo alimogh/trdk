d3a09e821602e8eb504c8a5d7b99f0bec0119c8f
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2014-04-29 22:53:10 +0700

    TRDK-115: C API empty project.

---
+++ sources/CApi/Prec.hpp
@@ -0,0 +1,22 @@
+/**************************************************************************
+ *   Created: 2014/04/29 22:51:08
+ *    Author: Eugene V. Palchukovsky
+ *    E-mail: eugene@palchukovsky.com
+ * -------------------------------------------------------------------
+ *   Project: Trading Robot Development Kit
+ *       URL: http://robotdk.com
+ * Copyright: Eugene V. Palchukovsky
+ **************************************************************************/
+
+#pragma once
+
+#include "Common/Assert.hpp"
+
+#include "Common/DisableBoostWarningsBegin.h"
+#include "Common/DisableBoostWarningsEnd.h"
+
+#include "Common/Common.hpp"
+
+#include "Core/Fwd.hpp"
+
+#include "Common/Assert.hpp"
