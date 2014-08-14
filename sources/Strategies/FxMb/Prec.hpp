70c9c5d069a670636bd3b3861366dac43db601c1
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2014-08-14 10:41:17 +0700

    Added strategy FxMb-module - "FX Morgan Benoit Strategies".

---
+++ sources/Strategies/FxMb/Prec.hpp
@@ -0,0 +1,20 @@
+/**************************************************************************
+ *   Created: 2014/08/14 10:01:50
+ *    Author: Eugene V. Palchukovsky
+ *    E-mail: eugene@palchukovsky.com
+ * -------------------------------------------------------------------
+ *   Project: Trading Robot Development Kit
+ *       URL: http://robotdk.com
+ * Copyright: Eugene V. Palchukovsky
+ **************************************************************************/
+
+#pragma once
+
+#include "Common/Assert.hpp"
+
+#include "Common/DisableBoostWarningsBegin.h"
+#include "Common/DisableBoostWarningsEnd.h"
+
+#include "Common/Common.hpp"
+
+#include "Common/Assert.hpp"
