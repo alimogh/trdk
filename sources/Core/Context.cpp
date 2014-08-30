382b11cbcd541f4f60176e7292735dc531b1a69b
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2014-08-30 10:25:59 +0700

    Added Dispatching Time Measurement. All from trdk::TimeMeasurement moved to trdk::Lib::TimeMeasurement.

--- sources/Core/Context.cpp
+++ sources/Core/Context.cpp
@@ -10,7 +10,6 @@
 
 #include "Prec.hpp"
 #include "Context.hpp"
-#include "TimeMeasurement.hpp"
 
 using namespace trdk;
 using namespace trdk::Lib;
@@ -40,153 +39,215 @@ Context::Log::~Log() {
 	//...//
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+
+	typedef TimeMeasurement::MilestonesStatAccum<
+			TimeMeasurement::numberOfStrategyMilestones>
+		StrategyMilestonesStatAccum;
+	typedef TimeMeasurement::MilestonesStatAccum<
+			TimeMeasurement::numberOfTradeSystemMilestones>
+		TradeSystemMilestonesStatAccum;
+	typedef TimeMeasurement::MilestonesStatAccum<
+			TimeMeasurement::numberOfDispatchingMilestones>
+		DispatchingMilestonesStatAccum;
+
+	class LatanReport : private boost::noncopyable {
+
+	private:
+
+		typedef boost::mutex Mutex;
+		typedef Mutex::scoped_lock Lock;
+		typedef boost::condition_variable StopCondition;
+
+	public:
+		
+		explicit LatanReport(trdk::Context::Log &log)
+				: m_log(log),
+				m_thread([&]{ThreadMain();}) {
+			//....//
+		}
+
+		~LatanReport() {
+			m_stopCondition.notify_all();
+			m_thread.join();
+		}
+
+	public:
+
+		TimeMeasurement::Milestones StartStrategyTimeMeasurement() {
+			return TimeMeasurement::Milestones(m_accums.strategy);
+		}
+
+		TimeMeasurement::Milestones StartTradeSystemTimeMeasurement() {
+			return TimeMeasurement::Milestones(m_accums.tradeSystem);
+		}
+
+		TimeMeasurement::Milestones StartDispatchingTimeMeasurement() {
+			return TimeMeasurement::Milestones(m_accums.dispatching);
+		}
+
+	private:
+
+		void ThreadMain() {
+
+			using namespace trdk::Lib::TimeMeasurement;
+
+			try {
+				
+				const auto reportPeriod = pt::seconds(30);
+				const fs::path logPath
+					= Lib::GetExeWorkingDir() / "logs" / "latan.log";
+				m_log.Info(
+					"Logging Latan to to file %1% with period %2%...",
+					boost::make_tuple(logPath, reportPeriod));
+
+				boost::filesystem::create_directories(logPath.branch_path());
+				std::ofstream log(
+					logPath.string().c_str(),
+					std::ios::ate | std::ios::app);
+				if (!log) {
+					throw Exception("Failed to open Latan report file");
+				}
+
+				log
+					<< std::endl
+					<< "=========================================================================="
+					<< std::endl << "Started at "
+					<< pt::microsec_clock::universal_time() << " with period "
+					<< reportPeriod << " (" << TRDK_BUILD_IDENTITY << ")."
+					<< std::endl;
+				{
+					typedef Milestones::Clock Clock;
+					const auto &test = [&](size_t period) {
+						const auto start = Clock::now();
+						boost::this_thread::sleep(pt::microseconds(period));
+						const auto now = Clock::now();
+						log
+							<< period  << " = " << (now - start).count() << " / "
+							<< Milestones::CalcPeriod(start, now);
+					};
+					log << "Test: ";
+					test(1000); log << ", ";
+					test(500); log << ", ";
+					test(100); log << ", ";
+					test(10); log << ", ";
+					test(1);
+					log << "." << std::endl;
+				}
+
+				Lock lock(m_mutex);
+				for (
+						size_t strategyIndex = 0,
+							tsIndex = 0,
+							dispatchingIndex = 0;
+						!m_stopCondition.timed_wait(lock, reportPeriod);
+						) {
+					DumpAccum<TimeMeasurement::StrategyMilestone>(
+						strategyIndex,
+						"Strategy",
+						*m_accums.strategy,
+						log);
+					DumpAccum<TimeMeasurement::TradeSystemMilestone>(
+						tsIndex,
+						"TradeSystem",
+						*m_accums.tradeSystem,
+						log);
+					DumpAccum<TimeMeasurement::DispatchingMilestone>(
+						dispatchingIndex,
+						"Dispatching",
+						*m_accums.dispatching,
+						log);
+				}
+
+			} catch (...) {
+				trdk::Log::RegisterUnhandledException(
+					__FUNCTION__,
+					__FILE__,
+					__LINE__,
+					false);
+				throw;
+			}
+		}
+
+		template<
+			typename TimeMeasurementMilestone,
+			typename MilestonesStatAccum>
+		void DumpAccum(
+					size_t &index,
+					const std::string &tag,
+					MilestonesStatAccum &accum,
+					std::ostream &destination) {
+
+			if (!accum.HasMeasures()) {
+				return;
+			}
+
+			++index;
+			
+			destination
+				<< "[" << tag << "." << index << "] "
+				<< pt::microsec_clock::universal_time() << ":"
+				<< std::endl;
+			size_t milestoneIndex = 0;
+			foreach (const auto &stat, accum.GetMilestones()) {
+				TimeMeasurementMilestone id
+					= static_cast<TimeMeasurementMilestone>(milestoneIndex++);
+				destination
+					<< "\t" << GetMilestoneName(id)
+					<< "\t" << stat
+					<< std::endl;
+			}
+
+			accum.Reset();
+
+		}
+
+
+	private:
+
+		trdk::Context::Log &m_log;
+
+		struct Accums {
+			
+			boost::shared_ptr<StrategyMilestonesStatAccum> strategy;
+			boost::shared_ptr<TradeSystemMilestonesStatAccum> tradeSystem;
+			boost::shared_ptr<DispatchingMilestonesStatAccum> dispatching;
+
+			Accums()
+					: strategy(new StrategyMilestonesStatAccum),
+					tradeSystem(new TradeSystemMilestonesStatAccum),
+					dispatching(new DispatchingMilestonesStatAccum) {
+				//...//
+			}
+
+		} m_accums;
+
+		Mutex m_mutex;
+		StopCondition m_stopCondition;
+		boost::thread m_thread;
+
+	};
+
+}
+
 //////////////////////////////////////////////////////////////////////////
 
 class Context::Implementation : private boost::noncopyable {
 
 public:
 
-	typedef TimeMeasurement::MilestonesStatAccum<
-			numberOfStrategyTimeMeasurementMilestones>
-		StrategyMilestonesStatAccum;
-	typedef TimeMeasurement::MilestonesStatAccum<
-			numberOfTradeSystemTimeMeasurementMilestones>
-		TradeSystemMilestonesStatAccum;
-
-public:
-
 	Context::Log m_log;
 	Params m_params;
 
-	boost::shared_ptr<StrategyMilestonesStatAccum> m_strategyMilestonesStatAccum;
-	boost::shared_ptr<TradeSystemMilestonesStatAccum> m_tradeSystemMilestonesStatAccum;
-	boost::atomic_bool m_latanReportThreadStopFlag;
-	boost::thread m_latanReportThread;
+	LatanReport m_latanReport;
 	
 	explicit Implementation(Context &context)
 			: m_log(context),
 			m_params(context),
-			m_latanReportThreadStopFlag(false),
-			m_strategyMilestonesStatAccum(new StrategyMilestonesStatAccum),
-			m_tradeSystemMilestonesStatAccum(new TradeSystemMilestonesStatAccum),
-			m_latanReportThread([&]() {ReportLatan();}) {
+			m_latanReport(m_log) {
 		//...//
-	}
-
-	~Implementation() {
-		m_latanReportThreadStopFlag = true;
-		m_latanReportThread.join();
-	}
-
-	void ReportLatan() {
-		
-		using namespace trdk::Lib::TimeMeasurement;
-		
-		try {
-
-			const auto reportPeriod = pt::seconds(30);
-			const fs::path logPath
-				= Lib::GetExeWorkingDir() / "logs" / "latan.log";
-			m_log.Info(
-				"Logging Latan to to file %1% with period %2%...",
-				boost::make_tuple(logPath, reportPeriod));
-
-			boost::filesystem::create_directories(logPath.branch_path());
-			std::ofstream log(
-				logPath.string().c_str(),
-				std::ios::ate | std::ios::app);
-			if (!log) {
-				throw Exception("Failed to open Latan report file");
-			}
-
-			log
-				<< std::endl
-				<< "=========================================================================="
-				<< std::endl << "Started at "
-				<< pt::microsec_clock::universal_time() << " with period "
-				<< reportPeriod << " (" << TRDK_BUILD_IDENTITY << ")."
-				<< std::endl;
-			{
-				typedef Milestones::Clock Clock;
-				const auto &test = [&](size_t period) {
-					const auto start = Clock::now();
-					boost::this_thread::sleep(pt::microseconds(period));
-					const auto now = Clock::now();
-					log
-						<< period  << " = " << (now - start).count() << " / "
-						<< Milestones::CalcPeriod(start, now);
-				};
-				log << "Test: ";
-				test(1000); log << ", ";
-				test(500); log << ", ";
-				test(100); log << ", ";
-				test(10); log << ", ";
-				test(1);
-				log << "." << std::endl;
-			}
-			
-			for (
-					size_t strategyIndex = 0, tsIndex = 0;
-					!m_latanReportThreadStopFlag;
-					) {
-
-				boost::this_thread::sleep(reportPeriod);
-	
-				if (m_strategyMilestonesStatAccum->HasMeasures()) {
-					DumpLatanAccum<StrategyTimeMeasurementMilestone>(
-						++strategyIndex,
-						"Strategy",
-						*m_strategyMilestonesStatAccum,
-						log);
-					m_strategyMilestonesStatAccum->Reset();
-				}
-
-				if (m_tradeSystemMilestonesStatAccum->HasMeasures()) {
-					DumpLatanAccum<TradeSystemTimeMeasurementMilestone>(
-						++tsIndex,
-						"TradeSystem",
-						*m_tradeSystemMilestonesStatAccum,
-						log);
-					m_tradeSystemMilestonesStatAccum->Reset();
-				}
-
-			}
-
-			log
-				<< "Stopped at "
-				<< pt::microsec_clock::universal_time() << "."
-				<< std::endl;
-			m_log.Info("Latan reporting stopped.");
-		
-		} catch (...) {
-			AssertFailNoException();
-			throw;
-		}
-	
-	}
-
-	template<typename TimeMeasurementMilestone, typename MilestonesStatAccum>
-	void DumpLatanAccum(
-				size_t index,
-				const std::string &tag,
-				const MilestonesStatAccum &milestonesStatAccum,
-				std::ostream &destination) {
-		destination
-			<< "[" << tag << "." << index << "] "
-			<< pt::microsec_clock::universal_time() << ":"
-			<< std::endl;
-		size_t milestoneIndex = 0;
-		foreach (
-				const auto &milestoneStat,
-				milestonesStatAccum.GetMilestones()) {
-			TimeMeasurementMilestone milestoneId
-				= static_cast<TimeMeasurementMilestone>(
-					milestoneIndex++);
-			destination
-				<< "\t" << GetTimeMeasurementMilestoneName(milestoneId)
-				<< "\t" << milestoneStat
-				<< std::endl;
-		}
 	}
 
 };
@@ -230,12 +291,15 @@ const Context::Params & Context::GetParams() const {
 }
 
 TimeMeasurement::Milestones Context::StartStrategyTimeMeasurement() const {
-	return TimeMeasurement::Milestones(m_pimpl->m_strategyMilestonesStatAccum);
+	return m_pimpl->m_latanReport.StartStrategyTimeMeasurement();
 }
 
 TimeMeasurement::Milestones Context::StartTradeSystemTimeMeasurement() const {
-	return TimeMeasurement::Milestones(
-		m_pimpl->m_tradeSystemMilestonesStatAccum);
+	return m_pimpl->m_latanReport.StartTradeSystemTimeMeasurement();
+}
+
+TimeMeasurement::Milestones Context::StartDispatchingTimeMeasurement() const {
+	return m_pimpl->m_latanReport.StartDispatchingTimeMeasurement();
 }
 
 //////////////////////////////////////////////////////////////////////////
