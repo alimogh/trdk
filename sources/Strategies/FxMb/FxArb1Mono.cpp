1a0a0e630af9bc4a357a832aa6702c503f1eb482
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2014-09-25 01:01:27 +0700

    Log at strategy runtime moved under DEV_VER.

--- sources/Strategies/FxMb/FxArb1Mono.cpp
+++ sources/Strategies/FxMb/FxArb1Mono.cpp
@@ -136,10 +136,11 @@ namespace trdk { namespace Strategies { namespace FxMb {
 						// at this time one of equation will return "true"
 						// again).
 
-						//! @todo remove in production, too slow
-// 						GetLog().Debug(
-// 							"Going to close orders on equation %1% / 12",
-// 							oppositeEquationIndex);
+#						ifdef DEV_VER
+ 							GetLog().Debug(
+ 								"Going to close orders on equation %1% / 12",
+	 							oppositeEquationIndex);
+#						endif
 
 						LogBrokersState(oppositeEquationIndex, b1, b2);
 
@@ -181,10 +182,11 @@ namespace trdk { namespace Strategies { namespace FxMb {
 				return;
 			}
 			
-			//! @todo remove in production, too slow
-// 			GetLog().Debug(
-// 				"Going to open orders on equation %1% / 12",
-// 				(bestEquationsIndex + 1));
+#			ifdef DEV_VER
+				GetLog().Debug(
+ 					"Going to open orders on equation %1% / 12",
+	 				(bestEquationsIndex + 1));
+#			endif
 			
 			OnEquation(bestEquationsIndex, b1, b2, timeMeasurement);
 		
