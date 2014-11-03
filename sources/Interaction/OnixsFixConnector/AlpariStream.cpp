6a0825180a4644f765cc25d034d59e3535007817
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2014-11-03 14:36:55 +0600

    Fixed seq. numbers control for all brokers but not for Alpari.

---
+++ sources/Interaction/OnixsFixConnector/AlpariStream.cpp
@@ -0,0 +1,68 @@
+/**************************************************************************
+ *   Created: 2014/11/03 13:53:42
+ *    Author: Eugene V. Palchukovsky
+ *    E-mail: eugene@palchukovsky.com
+ * -------------------------------------------------------------------
+ *   Project: Trading Robot Development Kit
+ *       URL: http://robotdk.com
+ * Copyright: Eugene V. Palchukovsky
+ **************************************************************************/
+
+#include "Prec.hpp"
+#include "FixStream.hpp"
+
+namespace fix = OnixS::FIX;
+
+using namespace trdk;
+using namespace trdk::Lib;
+using namespace trdk::Interaction::OnixsFixConnector;
+
+namespace trdk { namespace Interaction { namespace OnixsFixConnector {
+
+	class AlpariStream : public FixStream {
+
+	public:
+
+		explicit AlpariStream(
+					Context &context,
+					const std::string &tag,
+					const IniSectionRef &conf)
+				: FixStream(context, tag, conf) {
+			//...//
+		}
+
+		virtual ~AlpariStream() {
+			try {
+				GetSession().Disconnect();
+			} catch (...) {
+				AssertFailNoException();
+			}
+		}
+
+	protected:
+
+		virtual void OnLogout() {
+			//...//
+		}
+
+		virtual void OnReconnecting() {
+			// @todo Fix me!
+		}
+
+	};
+
+} } }
+
+////////////////////////////////////////////////////////////////////////////////
+
+TRDK_INTERACTION_ONIXSFIXCONNECTOR_API
+boost::shared_ptr<MarketDataSource>
+CreateAlpariStream(
+			Context &context,
+			const std::string &tag,
+			const IniSectionRef &configuration) {
+	return boost::shared_ptr<MarketDataSource>(
+		new AlpariStream(context, tag, configuration));
+}
+
+////////////////////////////////////////////////////////////////////////////////
