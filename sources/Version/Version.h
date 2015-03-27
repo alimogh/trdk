8ea7426db3c1ad16d69427f65b2a8056fa4473b9
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2015-03-27 09:47:44 +0600

    Version 1.1.0.36

--- sources/Version/Version.h
+++ sources/Version/Version.h
@@ -4,7 +4,7 @@
 #define TRDK_VERSION_MAJOR_HIGH	1
 #define TRDK_VERSION_MAJOR_LOW	1
 #define TRDK_VERSION_MINOR_HIGH	0
-#define TRDK_VERSION_MINOR_LOW	35
+#define TRDK_VERSION_MINOR_LOW	36
 
 #define TRDK_VERSION_BRANCH		"probitecapital"
 #define TRDK_VERSION_BRANCH_W	L"probitecapital"
