563d07d5fa0f457adfc35c53e9312cd2ff7829d9
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2015-06-22 06:08:14 +0600

    Version 2.0.0.63

--- sources/Version/Version.h
+++ sources/Version/Version.h
@@ -4,28 +4,28 @@
 #define TRDK_VERSION_MAJOR_HIGH	2
 #define TRDK_VERSION_MAJOR_LOW	0
 #define TRDK_VERSION_MINOR_HIGH	0
-#define TRDK_VERSION_MINOR_LOW	62
+#define TRDK_VERSION_MINOR_LOW	63
 
-#define TRDK_VERSION_BRANCH		"probitecapital"
-#define TRDK_VERSION_BRANCH_W	L"probitecapital"
+#define TRDK_VERSION_BRANCH		"mbcm"
+#define TRDK_VERSION_BRANCH_W	L"mbcm"
 
-#define TRDK_VENDOR		"Eugene V. Palchukovsky"
-#define TRDK_VENDOR_W	L"Eugene V. Palchukovsky"
+#define TRDK_VENDOR		"MBCMTRADING"
+#define TRDK_VENDOR_W	L"MBCMTRADING"
 
-#define TRDK_DOMAIN		"robotdk.com"
-#define TRDK_DOMAIN_W	L"robotdk.com"
+#define TRDK_DOMAIN		"mb-cm.com"
+#define TRDK_DOMAIN_W	L"mb-cm.com"
 
-#define TRDK_SUPPORT_EMAIL		"support@robotdk.com"
-#define TRDK_SUPPORT_EMAIL_W	L"support@robotdk.com"
+#define TRDK_SUPPORT_EMAIL		"support@mb-cm.com"
+#define TRDK_SUPPORT_EMAIL_W	L"support@mb-cm.com"
 
 #define TRDK_LICENSE_SERVICE_SUBDOMAIN		"licensing"
 #define TRDK_LICENSE_SERVICE_SUBDOMAIN_W	L"licensing"
 
-#define TRDK_NAME	"Probite Capital Trading Robot Development Kit"
-#define TRDK_NAME_W	L"Probite Capital Trading Robot Development Kit"
+#define TRDK_NAME	"MBCMTRADING Trading Robot Development Kit"
+#define TRDK_NAME_W	L"MBCMTRADING Trading Robot Development Kit"
 
-#define TRDK_COPYRIGHT		"Copyright 2015 (C) Eugene V. Palchukovsky, robotdk.com. All rights reserved."
-#define TRDK_COPYRIGHT_W	L"Copyright 2015 (C) Eugene V. Palchukovsky, robotdk.com. All rights reserved."
+#define TRDK_COPYRIGHT		"Copyright 2015 (C) MBCMTRADING, mb-cm.com. All rights reserved."
+#define TRDK_COPYRIGHT_W	L"Copyright 2015 (C) MBCMTRADING, mb-cm.com. All rights reserved."
 
 #define TRDK_CONCURRENCY_PROFILE (::trdk::Lib::Concurrency::PROFILE_HFT)
 
