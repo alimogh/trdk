de8a45ba7db19602517aa86edd3c9b3ee5b7d4ae
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2017-01-31 13:45:07 +0700

    Version 2.5.1

--- sources/Version/Version.h
+++ sources/Version/Version.h
@@ -3,7 +3,7 @@
 
 #define TRDK_VERSION_RELEASE	2
 #define TRDK_VERSION_BUILD	5
-#define TRDK_VERSION_STATUS	0
+#define TRDK_VERSION_STATUS	1
 
 #define TRDK_VERSION_BRANCH		"master"
 #define TRDK_VERSION_BRANCH_W	L"master"
