e8591f8ddda6651fbf1e1167921a3df8827f7855
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2017-08-21 11:39:04 +0700

    Fixed previous commit: security starts only with "last price" only if it is INDEX.

--- sources/Interaction/InteractiveBrokers/IbSecurity.cpp
+++ sources/Interaction/InteractiveBrokers/IbSecurity.cpp
@@ -12,6 +12,7 @@
 #include "IbSecurity.hpp"
 
 using namespace trdk;
+using namespace trdk::Lib;
 using namespace trdk::Interaction;
 using namespace trdk::Interaction::InteractiveBrokers;
 namespace ib = trdk::Interaction::InteractiveBrokers;
@@ -23,5 +24,15 @@ ib::Security::Security(Context &context,
     : Base(context,
            symbol,
            source,
-           SupportedLevel1Types().set(LEVEL1_TICK_LAST_PRICE)),
+           symbol.GetSecurityType() == SECURITY_TYPE_INDEX
+               // Custom branch for Mrigesh Kejriwal uses NIFTY index which has
+               // no bid and ask prices: "there is no bid and ask data for nifty
+               // as nifty itself is just an index. It is not traded. you will
+               // get bid and ask data for nifty futures. For our purposes last
+               // traded data is good enough".
+               ? SupportedLevel1Types().set(LEVEL1_TICK_LAST_PRICE)
+               : SupportedLevel1Types()
+                     .set(LEVEL1_TICK_LAST_PRICE)
+                     .set(LEVEL1_TICK_BID_PRICE)
+                     .set(LEVEL1_TICK_ASK_PRICE)),
       m_isTestSource(isTestSource) {}
