881722668d9246a9df26a5c6808b831825495edb
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2017-09-11 16:04:30 +0700

    Version 1.0.25

--- sources/Version/Version.h
+++ sources/Version/Version.h
@@ -3,7 +3,7 @@
 
 #define TRDK_VERSION_RELEASE	1
 #define TRDK_VERSION_BUILD	0
-#define TRDK_VERSION_STATUS	24
+#define TRDK_VERSION_STATUS	25
 
 #define TRDK_VERSION_BRANCH		"MrigeshKejriwal"
 #define TRDK_VERSION_BRANCH_W	L"MrigeshKejriwal"
