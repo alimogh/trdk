2046b5901c11c20a56fe5ea81da59501481d01c9
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2017-10-12 11:21:52 +0700

    Version of master branch set "1.0.1" as master branch will never released and always will be base of all other branches.

--- sources/Version/Version.h
+++ sources/Version/Version.h
@@ -1,9 +1,9 @@
 
 #pragma once
 
-#define TRDK_VERSION_RELEASE	2
-#define TRDK_VERSION_BUILD	5
-#define TRDK_VERSION_STATUS	3
+#define TRDK_VERSION_RELEASE	1
+#define TRDK_VERSION_BUILD	0
+#define TRDK_VERSION_STATUS	1
 
 #define TRDK_VERSION_BRANCH		"master"
 #define TRDK_VERSION_BRANCH_W	L"master"
