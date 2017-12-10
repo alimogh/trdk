94b032c0086b1fa589b5013b7064997c67ce8654
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2017-12-11 00:11:51 +0700

    [brutus] Customizing branch - branch defaults for REST module pulling intervals.

--- sources/Interaction/Rest/PullingSettings.cpp
+++ sources/Interaction/Rest/PullingSettings.cpp
@@ -18,9 +18,9 @@ using namespace trdk::Interaction::Rest;
 namespace pt = boost::posix_time;
 
 namespace {
-const pt::time_duration &minInterval = pt::microseconds(1);
+const pt::time_duration &minInterval = pt::seconds(3);
 const pt::time_duration &defailtInterval = pt::seconds(1);
-const size_t defaultPriceRequestFrequency = 1;
+const size_t defaultPriceRequestFrequency = 15;
 const size_t defaultActualOrdersRequestFrequency = 1;
 const size_t defaultAllOrdersRequestFrequency = 60;
 const size_t defaultBalancesRequestFrequency = 120;
