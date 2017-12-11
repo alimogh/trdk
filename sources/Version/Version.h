7bd748abd63de3bc15ed09cdb7803f3f7936cbfd
Eugene V. Palchukovsky <eugene@palchukovsky.com>
2017-12-11 11:43:22 +0700

    Version 1.4.1

--- sources/Version/Version.h
+++ sources/Version/Version.h
@@ -2,8 +2,8 @@
 #pragma once
 
 #define TRDK_VERSION_RELEASE	1
-#define TRDK_VERSION_BUILD	3
-#define TRDK_VERSION_STATUS	8
+#define TRDK_VERSION_BUILD	4
+#define TRDK_VERSION_STATUS	1
 
 #define TRDK_VERSION_BRANCH		"robot"
 #define TRDK_VERSION_BRANCH_W	L"robot"
